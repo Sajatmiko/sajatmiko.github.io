415d897f0249e3e63b02625d64e7ad3927850e46
st85 <sajatmiko@gmail.com>
2020-11-04 11:21:55 +0700

    Initial commit

---
+++ third/precache-manifest.6d6185d24bc4489e841da2d2791ba1ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e31d68f1decfea5f758ce494e0953db9",
+    "url": "/second/index.html"
+  },
+  {
+    "revision": "ab8f53f09e00496f83e2",
+    "url": "/second/static/css/main.0300407e.chunk.css"
+  },
+  {
+    "revision": "04e4e9ea9197dae1627f",
+    "url": "/second/static/js/2.4f7b829e.chunk.js"
+  },
+  {
+    "revision": "72233f91859a49ec6af260af9714cdf4",
+    "url": "/second/static/js/2.4f7b829e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab8f53f09e00496f83e2",
+    "url": "/second/static/js/main.4bc9a32b.chunk.js"
+  },
+  {
+    "revision": "18f4af0a41c44554664e",
+    "url": "/second/static/js/runtime-main.aa5aaffd.js"
+  }
+]);
