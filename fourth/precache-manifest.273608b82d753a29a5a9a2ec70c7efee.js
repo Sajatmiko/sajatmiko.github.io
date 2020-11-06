4afb3cfad910b3522a81eca4ea219c5ee7df9efe
st85 <sajatmiko@gmail.com>
2020-11-06 12:33:22 +0700

    fourth

---
+++ fourth/precache-manifest.273608b82d753a29a5a9a2ec70c7efee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "931d46c15bdff1b31b36973c1241322b",
+    "url": "/fourth/index.html"
+  },
+  {
+    "revision": "7ccc1e5b70cfca0bbc6e",
+    "url": "/fourth/static/css/main.444508ef.chunk.css"
+  },
+  {
+    "revision": "aaead67e7c9588974440",
+    "url": "/fourth/static/js/2.cfad848e.chunk.js"
+  },
+  {
+    "revision": "5a5359a5ef643df5eb22ce3de908e105",
+    "url": "/fourth/static/js/2.cfad848e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ccc1e5b70cfca0bbc6e",
+    "url": "/fourth/static/js/main.04d93ac5.chunk.js"
+  },
+  {
+    "revision": "84242ae2b5220d836c68",
+    "url": "/fourth/static/js/runtime-main.edbf6022.js"
+  }
+]);
